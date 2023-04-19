0dec5b560c4b60ebc3f9295eff21dcae4d69e8f9
munseohee <nansh9815@naver.com>
2023-04-19 19:45:47 +0900

    feat: TDD 프로젝트

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const PORT = 5000;
+const app = express();
+const productRoutes = require("./routes");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://admin:admin@cluster0.nvqlfgk.mongodb.net/test", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log(err));
+app.use(express.json());
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
---
+++ test/unit/products.test.js
@@ -0,0 +1,3 @@
+test("", () => {
+  expect(2 + 2).not.toBe(5);
+});
