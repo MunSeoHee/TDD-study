43b1b3d6aa0ae8c6ca5ed0677339376a6c79ae1b
MunSeoHee <nansh9815@naver.com>
2023-04-23 14:40:24 +0900

    feat: 응답값 테스트코드 작성 및 코드 작성

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -17,10 +17,15 @@ describe("product controller create", () => {
     req.body = newProduct;
   })
   it("should have a createProduct function", () => {
-      expect(typeof productController.createProduct).toBe("function")
+    expect(typeof productController.createProduct).toBe("function")
   })
   it("should call productModel.create", () => {
     productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
+  })
+  it("should return 201 response code", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
   })
 })
