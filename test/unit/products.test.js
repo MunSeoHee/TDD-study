0d4baf10be605e250e4eac92f44c2dbd61e92d55
MunSeoHee <nansh9815@naver.com>
2023-04-23 14:14:39 +0900

    feat: node-mocks-http를 이용한 create 테스트코드

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,7 +1,19 @@
 const productController = require('../../controller/products');
+const productModel = require('../../models/Product');
+const httpMock = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
+productModel.create = jest.fn();
 
 describe("product controller create", () => {
   it("should have a createProduct function", () => {
       expect(typeof productController.createProduct).toBe("function")
+  })
+  it("should call productModel.create", () => {
+    let req = httpMock.createRequest();
+    let res = httpMock.createResponse();
+    let next = null;
+    req.body = newProduct;
+    productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(newProduct);
   })
 })
